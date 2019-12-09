8c463864326e5d0a2f2649ac6075ba15c35c8c35
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-12-09 00:14:10 -0500

    resolved issue

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use("/api/v1", api);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/../../client/build/index.html");
+    res.sendFile("/../../client/build/index.html");
   });
 }
 app.use(middlewares.notFound);
