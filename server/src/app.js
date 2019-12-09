d05a065985543c888977903b7871fe8968285d96
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-12-09 00:31:01 -0500

    import path

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 
