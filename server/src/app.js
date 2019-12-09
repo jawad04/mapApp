2bad01a066c9913e6b58161b1bf73d3b0146d74e
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-12-09 00:36:31 -0500

    fixed style loading issue

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "client/build"));
   app.get("*", (req, res) => {
     res.sendFile("index.html", {
-      root: path.join(__dirname, "../../client/build")
+      root: path.join(__dirname, "/../../client/build")
     });
   });
 }
