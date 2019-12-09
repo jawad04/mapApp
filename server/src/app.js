a4715b0486f9a62d639e9268b66a697fb955aa4f
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-12-08 23:47:26 -0500

    fixed app.use

--- server/src/app.js
+++ server/src/app.js
@@ -18,14 +18,13 @@ app.use(cors());
 
 app.use("/api/v1", api);
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 module.exports = app;
