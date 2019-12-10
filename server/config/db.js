319e8ea8d4f55900ed7b9978f34d47d13baa7105
Jawad Muhammadi <38444229+jawad04@users.noreply.github.com>
2019-12-09 21:12:41 -0500

    Delete db.js

--- server/config/db.js
+++
@@ -1,7 +0,0 @@
-const monk = require("monk");
-const config = require("config");
-const db = monk(
-  "mongodb+srv://mapApp:C7zkPxKIsVTB7vhf@cluster0-iyqco.mongodb.net/test?retryWrites=true&w=majority"
-);
-
-module.exports = db;
